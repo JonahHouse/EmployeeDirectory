d87593d31a59631c2ddccef88d61d99d4b498942
Jonah Householder <jonahhouseholder@outlook.com>
2020-06-24 09:12:30 -0700

    Adding sort functionality

--- src/App.js
+++ src/App.js
@@ -1,55 +1,89 @@
-import React, { Component } from 'react';
-import './App.css';
-import Search from './Components/Search'
-import TableRow from './Components/TableRow'
-import data from './Components/db/employee.json'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React, { Component } from "react";
+import "./App.css";
+import Search from "./Components/Search";
+import TableRow from "./Components/TableRow";
+import data from "./Components/db/employee.json";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends Component {
-
-
   state = {
+    searchQuery: "",
     searchResults: [],
-    searchOption: 'first_name'
-  }
+    searchOption: "first_name",
+    sortOption: "first_name",
+  };
 
-  handleSearchOption = (event) => {
-    this.setState({ searchOption: [event.target.value] })
-  }
+  handleSortOption = async (event) => {
+    await this.setState({
+      sortOption: event.target.value,
+    });
+    this.handleSearch();
+  };
+
+  handleSearchOption = async (event) => {
+    await this.setState({
+      searchOption: event.target.value,
+    });
+    this.handleSearch();
+  };
+
+  handleSearchChange = async (event) => {
+    await this.setState({
+      searchQuery: event.target.value,
+    });
+    this.handleSearch();
+  };
 
   handleSearch = (event) => {
-    const newData = data.filter(employees => {
+    const newData = data.filter((employees) => {
       if (this.state.searchOption == "first_name") {
-        return employees.first_name.toLowerCase().match(event.target.value)
+        return employees.first_name.toLowerCase().match(this.state.searchQuery);
       } else if (this.state.searchOption == "last_name") {
-        return employees.last_name.toLowerCase().match(event.target.value)
+        return employees.last_name.toLowerCase().match(this.state.searchQuery);
       } else if (this.state.searchOption == "email") {
-        return employees.email.toLowerCase().match(event.target.value)
+        return employees.email.toLowerCase().match(this.state.searchQuery);
       } else if (this.state.searchOption == "role") {
-        return employees.role.toLowerCase().match(event.target.value)
+        return employees.role.toLowerCase().match(this.state.searchQuery);
       }
     });
+    if (this.state.sortOption == "first_name") {
+      newData.sort((a, b) => {
+        let nameA = a.first_name.toUpperCase();
+        let nameB = b.first_name.toUpperCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+      });
+    } else if (this.state.sortOption == "last_name") {
+      newData.sort((a, b) => {
+        let nameA = a.last_name.toUpperCase();
+        let nameB = b.last_name.toUpperCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+      });
+    }
     this.setState({
-      searchResults: newData
-    })
-  }
-
-
+      searchResults: newData,
+    });
+  };
 
   render() {
-
     return (
       <>
         <Router>
           <Route path="/">
             <Search
-              handleSearchOption={this.handleSearchOption}
-              handleSearch={this.handleSearch} />
+              handleSearchChange={this.handleSearchChange}
+              handleSearch={this.handleSearch}
+              handleSortOption={this.handleSortOption}
+            />
             <table className="table">
               <thead>
                 <tr>
@@ -60,16 +94,15 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-                {
-                  this.state.searchResults.map((row, index) =>
-                    <TableRow
-                      firstName={row.first_name}
-                      lastName={row.last_name}
-                      email={row.email}
-                      role={row.role}
-                      key={index} />
-                  )
-                }
+                {this.state.searchResults.map((row, index) => (
+                  <TableRow
+                    firstName={row.first_name}
+                    lastName={row.last_name}
+                    email={row.email}
+                    role={row.role}
+                    key={index}
+                  />
+                ))}
               </tbody>
             </table>
           </Route>
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,27 +1,55 @@
-import React from 'react';
-import { Input } from 'reactstrap';
+import React from "react";
+import { Input } from "reactstrap";
 
-const Search = props => {
+const Search = (props) => {
   return (
     <>
       <div className="w-25" style={{ margin: "40px auto" }}>
         <div className="input-group mb-3">
           <div className="input-group-prepend">
-            <label className="input-group-text" htmlFor="inputGroupSelect01">Options</label>
+            <label className="input-group-text" htmlFor="inputGroupSelect01">
+              Search By
+            </label>
           </div>
-          <select className="custom-select" id="inputGroupSelect01" onChange={props.handleSearchOption}>
-            <option defaultValue value="first_name">First Name</option>
+          <select
+            className="custom-select"
+            id="inputGroupSelect01"
+            onChange={props.handleSearchOption}
+          >
+            <option defaultValue value="first_name">
+              First Name
+            </option>
             <option value="last_name">Last Name</option>
             <option value="email">Email</option>
             <option value="role">Role</option>
           </select>
         </div>
-        <div className="input-group" >
-          <Input onChange={props.handleSearch} type="text" className="form-control" aria-describedby="button-addon2"></Input>
+        <div className="input-group mb-3">
+          <Input
+            onChange={props.handleSearchChange}
+            type="text"
+            className="form-control"
+            aria-describedby="button-addon2"
+          ></Input>
+        </div>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <label className="input-group-text" htmlFor="inputGroupSelect01">
+              Sort By
+            </label>
+          </div>
+          <select
+            className="custom-select"
+            id="inputGroupSelect02"
+            onChange={props.handleSortOption}
+          >
+            <option value="first_name">First Name</option>
+            <option value="last_name">Last Name</option>
+          </select>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Search;
